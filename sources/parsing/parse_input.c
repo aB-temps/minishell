ef49de101816a2b6c703dafeeb3311ebe8a77d86
aB-temps <abetemps@student.42lyon.fr>
2025-06-06 14:02:01 +0200

    ✨ feat(lexing): WIP

--- sources/parsing/parse_input.c
+++ sources/parsing/parse_input.c
@@ -16,7 +16,7 @@ t_input	*parse_input(char *line)
 		return ((void *)0);
 	tokenize_input(input, line);
 	print_input(input);
-	// format_input(input);
-	// print_input(input);
+	format_input(input);
+	print_input(input);
 	return (input);
 }
