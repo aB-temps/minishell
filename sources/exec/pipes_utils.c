6495bd84904fa4526ace61e807d5e9e0f1e5f6b1
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-06-27 16:03:35 +0200

    🏗️  refactor(pipes_utils): reparation of the prepare_pipe function to handle single command case

--- sources/exec/pipes_utils.c
+++ sources/exec/pipes_utils.c
@@ -113,7 +113,9 @@ void	close_and_swap_pipes(t_fd *fd)
 
 void	prepare_pipe(t_exec *exec, t_fd *fd, int i)
 {
-	if (i == 0)
+	if (exec->cmd_count == 1)
+		return ;
+	else if (i == 0)
 		first_cmd(fd, exec->infile);
 	else if (i == exec->cmd_count - 1)
 		last_cmd(fd, exec->outfile);
