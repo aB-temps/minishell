0a02418a84be848c20593e184db3e2caed9c4cc8
aB-temps <abetemps@student.42lyon.fr>
2025-06-30 04:45:55 +0200

    🔧 fix(env_var): adapted code to fit bash behavior

--- sources/token_formatting/format_env_var.c
+++ sources/token_formatting/format_env_var.c
@@ -1,6 +1,5 @@
 #include "token_formatting.h"
 
-// UGLY TO REFACTOR
 static char	*get_raw_var_name(char *s)
 {
 	char	*raw_var_name;
@@ -11,11 +10,10 @@ static char	*get_raw_var_name(char *s)
 	while (s[start] && s[start] != '$')
 		start++;
 	printf("raw\nstart => %zu / %c\n", start, s[start]);
-	end = start + 1 + is_quote(s[start + 1]);
+	end = start + 1;
 	while (s[end] && s[end] != '$' && !is_whitespace(s[end])
 		&& !is_quote(s[end]))
 		end++;
-	end += is_quote(s[end]);
 	printf("end => %zu / %c\n", end, s[end]);
 	raw_var_name = ft_strndup(s + start, end - start);
 	if (!raw_var_name)
@@ -30,7 +28,7 @@ static char	*get_var_name(char *s)
 	size_t	end;
 	char	*var_name;
 
-	start = 1 + is_quote(s[1]);
+	start = 1;
 	end = start;
 	printf("var\nstart => %zu / %c\n", start, s[start]);
 	while (s[end] && !is_quote(s[end]))
@@ -43,7 +41,7 @@ static char	*get_var_name(char *s)
 	return (var_name);
 }
 
-static char	*get_var_value(char *var_name, int exit_status)
+static char	*get_var_value(char *var_name, int exit_status, int *cursor)
 {
 	char	*var_value;
 
@@ -53,6 +51,11 @@ static char	*get_var_value(char *var_name, int exit_status)
 		if (!var_value)
 			return ((void *)0);
 	}
+	else if (!ft_strlen(var_name))
+	{
+		(*cursor)++;
+		return("$");
+	}
 	else
 		var_value = getenv(var_name);
 	if (!var_value)
@@ -61,7 +64,8 @@ static char	*get_var_value(char *var_name, int exit_status)
 	return (var_value);
 }
 
-static char	*substitute_env_var_occurences(char *s, int exit_status)
+static char	*substitute_env_var_occurences(char *s, int exit_status,
+		int *cursor)
 {
 	char	*ns;
 	char	*var_name;
@@ -69,14 +73,14 @@ static char	*substitute_env_var_occurences(char *s, int exit_status)
 	char	*var_value;
 
 	printf("S => '%s'\n\n", s);
-	ns = (void *)0; // UGLY TO REFACTOR
+	ns = (void *)0;
 	raw_var_name = get_raw_var_name(s);
 	if (!raw_var_name)
 		return ((void *)0);
 	var_name = get_var_name(raw_var_name);
 	if (!var_name)
 		return ((void *)0);
-	var_value = get_var_value(var_name, exit_status);
+	var_value = get_var_value(var_name, exit_status, cursor);
 	if (!var_value)
 		return ((void *)0);
 	ns = str_replace(s, raw_var_name, var_value);
@@ -93,13 +97,15 @@ void	format_env_var(t_input *input, t_token *array, ssize_t *i)
 {
 	char	*content;
 	char	*new_content;
+	int		cursor;
 
+	cursor = 0;
 	content = substitute_env_var_occurences(array[*i].raw_content,
-			input->last_exit_status);
-	while (content && ft_strchr(content, '$'))
+			input->last_exit_status, &cursor);
+	while (content && ft_strchr(content + cursor, '$'))
 	{
 		new_content = substitute_env_var_occurences(content,
-				input->last_exit_status);
+				input->last_exit_status, &cursor);
 		free(content);
 		content = new_content;
 	}
