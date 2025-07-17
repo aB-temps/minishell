43173b0e1e6e774499115682c2a75f4fd03650da
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-07-17 02:16:53 +0200

    🔧 refactor(exec): replace magic number with STDERR_FILENO for better readability in ft_exit

--- sources/builtins/ft_exit.c
+++ sources/builtins/ft_exit.c
@@ -4,6 +4,6 @@
 void	ft_exit(t_input *input, t_exec *exec, t_fd *fd)
 {
 	if (exec->cmd_count == 1)
-		ft_putendl_fd("exit", 2);
+		ft_putendl_fd("exit", STDERR_FILENO);
 	exit_exec(input, exec, fd);
 }
