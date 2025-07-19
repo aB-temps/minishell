8ee6f8bca260ae97d023f95f2008ad89661ed00f
Enzo <enzochevrier@icloud.com>
2025-07-19 16:25:55 +0200

    🔧 refactor(exec): update free_child function to ensure proper memory management by freeing additional resources

--- sources/exec/exec_process.c
+++ sources/exec/exec_process.c
@@ -6,7 +6,7 @@
 /*   By: enzo <enzo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 15:51:50 by enzo              #+#    #+#             */
-/*   Updated: 2025/07/19 15:51:51 by enzo             ###   ########.fr       */
+/*   Updated: 2025/07/19 16:24:55 by enzo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@
 int	free_child(t_exec *exec, t_input *input)
 {
 	free(exec->cmd_path);
-	clear_vector(&input->v_tokens);
-	free(input);
 	free(exec->pid_child);
+	clear_vector(&input->v_tokens);
+	free(input->env->array);
+	ft_lstclear(&input->env->list, free);
+	free(input->prompt);
+	free(input);
 	return (127);
 }
 
