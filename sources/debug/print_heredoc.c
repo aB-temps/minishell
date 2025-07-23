e59e878cae6593ff5e6216c6390dcd8e3f936c5e
aB-temps <abetemps@student.42lyon.fr>
2025-07-23 17:54:04 +0200

    🔧 fix(print_heredoc): dup read fd in debug

--- sources/debug/print_heredoc.c
+++ sources/debug/print_heredoc.c
@@ -2,18 +2,21 @@
 
 void	print_token_heredoc(t_token token)
 {
+	int		fd;
 	char	*line;
 
 	printf("fds[%s%i%s|%s%i%s]\n'", BOLD, ((int *)token.formatted_content)[0],
 		R_ALL, BOLD, ((int *)token.formatted_content)[1], R_ALL);
-	line = get_next_line(((int *)token.formatted_content)[1]);
+	fd = dup(((int *)token.formatted_content)[1]);
+	line = get_next_line(fd);
 	while (line)
 	{
 		printf("%s%s%s", DIM, line, R_ALL);
 		free(line);
-		line = get_next_line(((int *)token.formatted_content)[1]);
+		line = get_next_line(fd);
 	}
 	if (line)
 		free(line);
 	printf("'\n");
+	close(fd);
 }
