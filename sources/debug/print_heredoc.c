4b83f7e3ac67c1f11523d74d7ec29dec550159fa
aB-temps <abetemps@student.42lyon.fr>
2025-07-24 17:29:11 +0200

    🔧 fix(print_heredoc): printing inside

--- sources/debug/print_heredoc.c
+++ sources/debug/print_heredoc.c
@@ -2,21 +2,19 @@
 
 void	print_token_heredoc(t_token token)
 {
-	int		fd;
-	char	*line;
+	// char	*line;
 
 	printf("fds[%s%i%s|%s%i%s]\n'", BOLD, ((int *)token.formatted_content)[0],
 		R_ALL, BOLD, ((int *)token.formatted_content)[1], R_ALL);
-	fd = dup(((int *)token.formatted_content)[1]);
-	line = get_next_line(fd);
-	while (line)
-	{
-		printf("%s%s%s", DIM, line, R_ALL);
-		free(line);
-		line = get_next_line(fd);
-	}
-	if (line)
-		free(line);
-	printf("'\n");
-	close(fd);
+	// line = get_next_line(((int *)token.formatted_content)[1]);
+	// while (line)
+	// {
+	// 	printf("%s%s%s", DIM, line, R_ALL);
+	// 	free(line);
+	// 	line = get_next_line(((int *)token.formatted_content)[1]);
+	// }
+	// if (line)
+	// 	free(line);
+	// printf("'\n");
+	// close(((int *)token.formatted_content)[1]);
 }
