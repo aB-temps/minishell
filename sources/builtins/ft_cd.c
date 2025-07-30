14d7fda9fc9bcac0e636509b3dbfc1cd8bc4c8f0
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-07-30 03:53:47 +0200

    🔧 fix(builtins): improve error handling in ft_cd and update file header timestamp

--- sources/builtins/ft_cd.c
+++ sources/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 21:04:11 by enchevri          #+#    #+#             */
-/*   Updated: 2025/07/29 22:38:35 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/07/30 03:53:37 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,34 +38,51 @@ int	ft_cd(char **cmd, t_input *input)
 	char	*new_wd;
 	char	*old_pwd;
 	char	**to_exp;
+	int		ret;
 
 	new_wd = NULL;
+	old_pwd = NULL;
+	old_pwd = getcwd(old_pwd, 0);
+	if (!old_pwd)
+		return (1);
 	if (cmd[1] == NULL)
-		handle_cd_alone(input);
+	{
+		ret = handle_cd_alone(input);
+		if (ret != 0)
+		{
+			free(old_pwd);
+			return (ret);
+		}
+	}
 	else if (cmd[2] != NULL)
 	{
 		ft_putstr_fd("cd: too many arguments\n", 2);
+		free(old_pwd);
 		return (1);
 	}
 	else if (chdir(cmd[1]) == -1)
 	{
 		perror(cmd[1]);
+		free(old_pwd);
 		return (1);
 	}
 	to_exp = ft_calloc(4, sizeof(char *));
 	if (!to_exp)
-		exit_minishell(input, EXIT_FAILURE);
-	old_pwd = getcwd(new_wd, PATH_MAX);
-	if (!old_pwd)
 	{
-		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-		ft_putstr_fd(cmd[1], STDERR_FILENO);
-		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
-		return (free_tab_return_int(to_exp, 1));
+		free(old_pwd);
+		exit_minishell(input, EXIT_FAILURE);
+	}
+	new_wd = getcwd(new_wd, PATH_MAX);
+	if (!new_wd)
+	{
+		free(old_pwd);
+		free_tab_return_null(to_exp);
+		ft_putstr_fd("minishell: cd: error getting current directory\n",
+			STDERR_FILENO);
+		return (1);
 	}
 	to_exp[0] = ft_strdup("export");
 	to_exp[1] = ft_strjoin("OLDPWD=", old_pwd);
-	new_wd = getcwd(new_wd, PATH_MAX);
 	to_exp[2] = ft_strjoin("PWD=", new_wd);
 	to_exp[3] = NULL;
 	ft_export(to_exp, input);
