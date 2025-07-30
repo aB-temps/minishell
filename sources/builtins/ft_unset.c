4680f1f7a52192c3cf3d3daa5da9fc6d499e1af6
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-07-30 04:06:17 +0200

    🔧 fix(builtins): update ft_unset logic for environment variable deletion and refresh file header timestamp

--- sources/builtins/ft_unset.c
+++ sources/builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 18:14:15 by abetemps          #+#    #+#             */
-/*   Updated: 2025/07/29 22:25:11 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/07/30 04:05:47 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,15 @@ int	ft_unset(char **cmd_args, t_input *input)
 	{
 		elem = find_env_var(cmd_args[i++], input->env->list);
 		prev = lstgetprev(input->env->list, elem);
-		if (!prev)
-		{
-			input->env->list = elem->next;
-			clear_env_list_elem(elem->content);
-			free(elem);
-		}
-		else
-			ft_lstdelone(prev, elem, &clear_env_list_elem);
 	}
+	if (!prev && elem)
+	{
+		input->env->list = elem->next;
+		clear_env_list_elem(elem->content);
+		free(elem);
+	}
+	else if (elem)
+		ft_lstdelone(prev, elem, &clear_env_list_elem);
 	update_env_array(input);
 	return (0);
 }
