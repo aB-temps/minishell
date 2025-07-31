d920ad81f04afb705721ed39f1901dc13c43c446
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-07-31 04:12:51 +0200

    update: remove unused variable

--- sources/env/find_env_var.c
+++ sources/env/find_env_var.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   find_env_var.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 18:15:29 by abetemps          #+#    #+#             */
-/*   Updated: 2025/07/26 18:15:37 by abetemps         ###   ########.fr       */
+/*   Updated: 2025/07/31 04:12:19 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 
 t_list	*find_env_var(char *key, t_list *env)
 {
-	size_t	i;
-
-	i = 0;
 	while (env)
 	{
 		if (!ft_strcmp(key, ((t_env_var *)env->content)->key))
