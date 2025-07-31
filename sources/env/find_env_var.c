900b7c4dabb7d261fff6aff81e65672ca39a59ad
aB-temps <abetemps@student.42lyon.fr>
2025-07-31 04:15:35 +0200

    ♻️  refactor: various fixes

--- sources/env/find_env_var.c
+++ sources/env/find_env_var.c
@@ -6,7 +6,7 @@
 /*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 18:15:29 by abetemps          #+#    #+#             */
-/*   Updated: 2025/07/26 18:15:37 by abetemps         ###   ########.fr       */
+/*   Updated: 2025/07/31 04:15:00 by abetemps         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 
 t_list	*find_env_var(char *key, t_list *env)
 {
-	size_t	i;
-
-	i = 0;
 	while (env)
 	{
 		if (!ft_strcmp(key, ((t_env_var *)env->content)->key))
