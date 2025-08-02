87c1074e61c1ceb377f33cc7155970e114f5097a
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-08-02 13:35:32 +0200

    🔧 fix: ensure statbuf is freed on directory check

--- sources/exec/check_if_dir.c
+++ sources/exec/check_if_dir.c
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 05:11:25 by enchevri          #+#    #+#             */
-/*   Updated: 2025/08/01 05:14:16 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/02 13:34:17 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ int	check_if_dir(t_input *input, char *path)
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd(path, 2);
 		ft_putstr_fd(": Is a directory\n", 2);
+		free(statbuf);
 		return (1);
 	}
 	free(statbuf);
