f504a026b7bb585ce3c31636e2c8cc4792f6c58f
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-08-08 13:42:13 +0200

    🔧 refactor: simplify pipe preparation logic by removing redundant functions and streamlining error handling

--- sources/exec/prepare_pipes.c
+++ sources/exec/prepare_pipes.c
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 01:18:36 by enchevri          #+#    #+#             */
-/*   Updated: 2025/08/06 23:30:15 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/08 13:41:02 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,88 +21,26 @@ static void	error_occured(t_input *input, t_exec *exec, char *error_msg)
 	exit_minishell(input, exec, errno);
 }
 
-static bool	first_cmd(t_exec *exec, int fd_infile)
-{
-	if (fd_infile != -1)
-	{
-		if (dup2(fd_infile, STDIN_FILENO) == -1)
-			return (false);
-	}
-	if (exec->block.io_fds[1] == -1)
-	{
-		if (dup2(exec->pipe_fds->fd2[1], STDOUT_FILENO) == -1)
-			return (false);
-	}
-	else
-	{
-		if (dup2(exec->block.io_fds[1], STDOUT_FILENO) == -1)
-			return (false);
-	}
-	return (true);
-}
-
-static bool	middle_cmd(t_exec *exec)
-{
-	if (exec->block.io_fds[0] == -1)
-	{
-		if (dup2(exec->pipe_fds->fd1[0], STDIN_FILENO) == -1)
-			return (false);
-	}
-	else
-	{
-		if (dup2(exec->block.io_fds[0], STDIN_FILENO) == -1)
-			return (false);
-	}
-	if (exec->block.io_fds[1] == -1)
-	{
-		if (dup2(exec->pipe_fds->fd2[1], STDOUT_FILENO) == -1)
-			return (false);
-	}
-	else if (dup2(exec->pipe_fds->fd2[1], STDOUT_FILENO) == -1)
-		return (false);
-	return (true);
-}
-
-static bool	last_cmd(t_exec *exec, int fd_outfile)
-{
-	if (dup2(exec->pipe_fds->fd1[0], STDIN_FILENO) == -1)
-		return (false);
-	if (fd_outfile != -1)
-	{
-		if (dup2(fd_outfile, STDOUT_FILENO) == -1)
-			return (false);
-	}
-	return (true);
-}
-
 void	prepare_pipes(t_input *input, t_exec *exec, size_t i)
 {
-	if (exec->block_qty == 1)
+	if (exec->block.io_fds[0] != -1)
 	{
-		if (exec->block.io_fds[0] != -1)
-		{
-			if (dup2(exec->block.io_fds[0], STDIN_FILENO) == -1)
-				error_occured(input, exec, "dup2");
-		}
-		if (exec->block.io_fds[1] != -1)
-		{
-			if (dup2(exec->block.io_fds[1], STDOUT_FILENO) == -1)
-				error_occured(input, exec, "dup2");
-		}
-	}
-	else if (i == 0)
-	{
-		if (!first_cmd(exec, exec->block.io_fds[0]))
+		if (dup2(exec->block.io_fds[0], STDIN_FILENO) == -1)
 			error_occured(input, exec, "dup2");
 	}
-	else if (i == exec->block_qty - 1)
+	else if (i > 0)
 	{
-		if (!last_cmd(exec, exec->block.io_fds[1]))
+		if (dup2(exec->pipe_fds->fd1[0], STDIN_FILENO) == -1)
 			error_occured(input, exec, "dup2");
 	}
-	else
+	if (exec->block.io_fds[1] != -1)
 	{
-		if (!middle_cmd(exec))
+		if (dup2(exec->block.io_fds[1], STDOUT_FILENO) == -1)
+			error_occured(input, exec, "dup2");
+	}
+	else if (i < exec->block_qty - 1)
+	{
+		if (dup2(exec->pipe_fds->fd2[1], STDOUT_FILENO) == -1)
 			error_occured(input, exec, "dup2");
 	}
 	close_fd_exec(exec);
