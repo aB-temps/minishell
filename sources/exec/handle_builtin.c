3916e758fb3a1553303f214e0a68d1a18ad13350
Enzo <enzochevrier@icloud.com>
2025-08-16 16:13:44 +0200

    fix: update author information and refactor redirection functions in handle_builtin and prepare_redir

--- sources/exec/handle_builtin.c
+++ sources/exec/handle_builtin.c
@@ -3,42 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   handle_builtin.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: enzo <enzo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 14:03:13 by enchevri          #+#    #+#             */
-/*   Updated: 2025/08/08 18:30:36 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/16 15:50:23 by enzo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include "exec.h"
-
-static void	apply_redirections_builtin(t_minishell *minishell, int *old_stdout,
-		int *old_stdin, int i)
-{
-	*old_stdout = dup(STDOUT_FILENO);
-	*old_stdin = dup(STDIN_FILENO);
-	if (!create_files_in_block(minishell->input, minishell->exec, i))
-		return ;
-	if (minishell->exec->block.io_fds[0] != -1)
-		dup2(minishell->exec->block.io_fds[0], STDIN_FILENO);
-	if (minishell->exec->block.io_fds[1] != -1)
-		dup2(minishell->exec->block.io_fds[1], STDOUT_FILENO);
-}
-
-static void	restore_redirections_builtin(int old_stdout, int old_stdin)
-{
-	if (old_stdout != -1)
-	{
-		dup2(old_stdout, STDOUT_FILENO);
-		close(old_stdout);
-	}
-	if (old_stdin != -1)
-	{
-		dup2(old_stdin, STDIN_FILENO);
-		close(old_stdin);
-	}
-}
 
 static void	exec_builtin(char **cmd, t_minishell *minishell)
 {
@@ -58,40 +31,47 @@ static void	exec_builtin(char **cmd, t_minishell *minishell)
 		ft_exit(cmd, minishell);
 }
 
-int	handle_builtin(t_input *input, t_exec *exec, int *pid_child, int i)
+static int	handle_builtin_pipeline(t_input *input, t_exec *exec,
+		int *pid_child, int i)
 {
-	int old_stdout;
-	int old_stdin;
-	t_minishell minishell;
+	t_minishell	minishell;
 
 	minishell.input = input;
 	minishell.exec = exec;
-
-	if (exec->block_qty > 1)
+	*pid_child = fork();
+	if (*pid_child == -1)
+		return (free_and_close_exec(&exec));
+	if (*pid_child == 0)
 	{
+		if (!create_files_in_block(input, exec, i))
+			exit_minishell(input, exec, 1);
 		prepare_redir(input, exec, i);
-
-		*pid_child = fork();
-		if (*pid_child == -1)
-		{
-			free_and_close_exec(&exec);
-			perror("fork");
-			return (FALSE);
-		}
-		if (*pid_child == 0)
-		{
-			exec_builtin(exec->block.cmd->cmd_args, &minishell);
-			exit(minishell.exec->return_error);
-		}
-		return (minishell.exec->return_error);
-	}
-	else
-	{
-		old_stdout = -1;
-		old_stdin = -1;
-		apply_redirections_builtin(&minishell, &old_stdout, &old_stdin, i);
 		exec_builtin(exec->block.cmd->cmd_args, &minishell);
-		restore_redirections_builtin(old_stdout, old_stdin);
-		return (minishell.exec->return_error);
+		exit_minishell(input, exec, minishell.input->last_exit_status);
 	}
+	return (minishell.exec->return_error);
+}
+
+static int	handle_builtin_single(t_input *input, t_exec *exec, int i)
+{
+	int			old_stdout;
+	int			old_stdin;
+	t_minishell	minishell;
+
+	minishell.input = input;
+	minishell.exec = exec;
+	old_stdout = -1;
+	old_stdin = -1;
+	apply_redirections_builtin(&minishell, &old_stdout, &old_stdin, i);
+	exec_builtin(exec->block.cmd->cmd_args, &minishell);
+	restore_redirections_builtin(old_stdout, old_stdin);
+	return (minishell.exec->return_error);
+}
+
+int	handle_builtin(t_input *input, t_exec *exec, int *pid_child, int i)
+{
+	if (exec->block_qty > 1)
+		return (handle_builtin_pipeline(input, exec, pid_child, i));
+	else
+		return (handle_builtin_single(input, exec, i));
 }
--- sources/exec/prepare_redir.c
+++ sources/exec/prepare_redir.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   prepare_redir.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: enzo <enzo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 01:18:36 by enchevri          #+#    #+#             */
-/*   Updated: 2025/08/08 14:32:16 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/16 15:55:54 by enzo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,34 @@
 #include "utils.h"
 #include <errno.h>
 #include <stdio.h>
+#include "builtins.h"
 
+void	apply_redirections_builtin(t_minishell *minishell, int *old_stdout,
+		int *old_stdin, int i)
+{
+	*old_stdout = dup(STDOUT_FILENO);
+	*old_stdin = dup(STDIN_FILENO);
+	if (!create_files_in_block(minishell->input, minishell->exec, i))
+		return ;
+	if (minishell->exec->block.io_fds[0] != -1)
+		dup2(minishell->exec->block.io_fds[0], STDIN_FILENO);
+	if (minishell->exec->block.io_fds[1] != -1)
+		dup2(minishell->exec->block.io_fds[1], STDOUT_FILENO);
+}
+
+void	restore_redirections_builtin(int old_stdout, int old_stdin)
+{
+	if (old_stdout != -1)
+	{
+		dup2(old_stdout, STDOUT_FILENO);
+		close(old_stdout);
+	}
+	if (old_stdin != -1)
+	{
+		dup2(old_stdin, STDIN_FILENO);
+		close(old_stdin);
+	}
+}
 static void	error_occured(t_input *input, t_exec *exec, char *error_msg)
 {
 	perror(error_msg);
@@ -23,6 +50,8 @@ static void	error_occured(t_input *input, t_exec *exec, char *error_msg)
 
 void	prepare_redir(t_input *input, t_exec *exec, size_t i)
 {
+	if (!create_files_in_block(input, exec, i))
+		return ;
 	if (exec->block.io_fds[0] != -1)
 	{
 		if (dup2(exec->block.io_fds[0], STDIN_FILENO) == -1)
