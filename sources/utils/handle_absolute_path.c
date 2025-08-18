38710b618338be8cf79d936692f408f61a59383f
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-08-18 05:20:46 +0200

    fix: reorder include statements and standardize code formatting in handle_absolute_path function

--- sources/utils/handle_absolute_path.c
+++ sources/utils/handle_absolute_path.c
@@ -6,23 +6,23 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 20:03:56 by enzo              #+#    #+#             */
-/*   Updated: 2025/08/18 04:25:35 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/18 05:20:35 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 char	*handle_absolute_path(t_exec *exec, char *cmd)
 {
-    if (!access(cmd, F_OK))
-    {
-        if (!access(cmd, X_OK))
-            return (ft_strdup(cmd));
-        exec->return_error = 126;
-        return (ft_strdup(cmd));
-    }
-    exec->return_error = 127;
-    return (NULL);
+	if (!access(cmd, F_OK))
+	{
+		if (!access(cmd, X_OK))
+			return (ft_strdup(cmd));
+		exec->return_error = 126;
+		return (ft_strdup(cmd));
+	}
+	exec->return_error = 127;
+	return (NULL);
 }
