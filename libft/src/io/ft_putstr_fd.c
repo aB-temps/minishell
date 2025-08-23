629e67c1c25b62ea20cfa740cc8578e493ab0832
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-08-23 15:06:52 +0200

    fix: simplify ft_putstr_fd implementation and improve null handling

--- libft/src/io/ft_putstr_fd.c
+++ libft/src/io/ft_putstr_fd.c
@@ -6,26 +6,23 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 18:10:20 by enchevri          #+#    #+#             */
-/*   Updated: 2025/03/10 13:55:02 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/23 15:01:19 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <unistd.h>
 
 int	ft_putstr_fd(char *s, int fd)
 {
-	int	count;
-	int	write_ret;
+	ssize_t	bytes_written;
+	size_t	len;
 
-	count = 0;
 	if (!s)
-		return (write(1, "(null)", 6));
-	while (s[count])
-	{
-		write_ret = write(fd, &s[count], 1);
-		if (write_ret == -1)
-			return (-1);
-		count++;
-	}
-	return (count);
+		return (write(fd, "(null)", 6));
+	len = ft_strlen(s);
+	bytes_written = write(fd, s, len);
+	if (bytes_written == -1 || (size_t)bytes_written != len)
+		return (-1);
+	return (bytes_written);
 }
