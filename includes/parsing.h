661fde828ef8515936a97dedaf5e7669d85b4ecf
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-08-29 07:10:56 +0200

    fix: update get_input function signature to remove unused argument

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 19:43:33 by abetemps          #+#    #+#             */
-/*   Updated: 2025/08/28 17:40:04 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/08/29 07:10:47 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <readline/readline.h>
 # include <stdlib.h>
 
-void	get_input(char **env, char **av);
+void	get_input(char **env);
 void	*parse_input(t_input *input);
 
 #endif
