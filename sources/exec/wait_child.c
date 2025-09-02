cd27ee4b3da081f3a9ba2d2402b322e548480268
Enzo Chevrier <enchevri@student.42lyon.fr>
2025-09-02 19:58:26 +0200

    fix: update GitHub Actions workflow for clearer log handling and fix return error when command not found

--- sources/exec/wait_child.c
+++ sources/exec/wait_child.c
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 18:49:29 by enchevri          #+#    #+#             */
-/*   Updated: 2025/09/02 17:13:30 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/09/02 19:55:03 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "wait.h"
 #include <unistd.h>
 
-void	handle_no_pid(void)
+static void	handle_no_pid(void)
 {
 	if (g_sig == SIGQUIT)
 		ft_putstr_fd("Quit (core dumped)", STDERR_FILENO);
@@ -23,7 +23,7 @@ void	handle_no_pid(void)
 	g_sig = 0;
 }
 
-int	handle_pid(t_exec *exec, int status, int *exit_status, size_t i)
+static void	handle_pid(int status, int *exit_status, int *last_block_has_pid)
 {
 	if (WIFSIGNALED(status))
 	{
@@ -32,37 +32,37 @@ int	handle_pid(t_exec *exec, int status, int *exit_status, size_t i)
 			ft_putstr_fd("Quit (core dumped)", STDERR_FILENO);
 		ft_putstr_fd("\n", STDERR_FILENO);
 	}
-	else if (WIFEXITED(status) && i == exec->block_qty - 1)
-	{
+	else if (WIFEXITED(status))
 		*exit_status = WEXITSTATUS(status);
-		return (1);
-	}
-	return (0);
+	*last_block_has_pid = 1;
 }
 
 void	wait_child(t_exec *exec, int *exit_status)
 {
 	size_t	i;
 	int		status;
-	int		last_has_pid;
+	int		last_block_has_pid;
 
 	i = 0;
-	last_has_pid = 0;
+	last_block_has_pid = 0;
 	while (i < exec->block_qty)
 	{
 		if (exec->pid_child[i] > 0)
 		{
 			waitpid(exec->pid_child[i], &status, 0);
 			if (i == exec->block_qty - 1)
-				if (handle_pid(exec, status, exit_status, i) == 0)
-					last_has_pid = 1;
+				handle_pid(status, exit_status, &last_block_has_pid);
+			else
+				last_block_has_pid = 1;
 		}
 		i++;
 	}
-	if (!last_has_pid)
+	if (!last_block_has_pid)
 	{
+		printf("COU\n");
 		*exit_status = 0;
 		if (g_sig != 0)
 			handle_no_pid();
 	}
+	g_sig = 0;
 }
